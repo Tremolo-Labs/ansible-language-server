9c76b5d039f40cd54c3a24dbaa76d156da52f8d4
Chad Cassady <chad.cassady@gmail.com>
2026-02-03 22:57:26 -0600

    moar hack

--- src/ansible_ls/utils/__init__.py
+++ src/ansible_ls/utils/__init__.py
@@ -1,5 +1,5 @@
 """Utility functions."""
 
-from .yaml_utils import get_node_at_position, parse_yaml
+from .parse_ansible import get_node_at_position, parse_ansible
 
-__all__ = ["get_node_at_position", "parse_yaml"]
+__all__ = ["get_node_at_position", "parse_ansible"]
